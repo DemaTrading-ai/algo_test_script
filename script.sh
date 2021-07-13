749ce0ee3ad954865d188046d90239417854428a
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-07-13 11:06:02 +0200

    [changes] - The script

--- script.sh
+++ script.sh
@@ -4,16 +4,16 @@ work () {
   local from=$1
   local to=$2
   local config=$3
+  local plots=$4
   targetdir=$(pwd)/output/$config
   docker run -v "$(pwd)/strategies:/usr/src/engine/strategies" \
     -v "$(pwd)/${config}:/usr/src/engine/config.json" \
     -v "$(pwd)/data/backtesting-data:/usr/src/engine/data/backtesting-data" \
-    --rm dematrading/engine:develop -from $from -to $to -plots=false  \
+    --rm dematrading/engine:develop -from $from -to $to -plots=$plots  \
     >>$targetdir/$config.txt
   sed -i 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g' $targetdir/$config.txt
 }
 
-echo "TEST"
 mkdir output
 for config in *.json
   do
@@ -23,14 +23,17 @@ for config in *.json
         mkdir "output/$config"
         if [ -n "$config" ]; then
           echo "BACKTESTING SINGLE CONFIG: ${config}\n\n\n"
-          work 20200601 20210625 $config
-          work 20210221 20210228 $config
-          work 20210415 20210424 $config
+          work 20190601 20210701 $config false
+          work 20200601 20210625 $config true
+          cp ./data/plots/ "output/$config/"
+          work 20210221 20210228 $config false
+          work 20210428 20210527 $config false
         fi
 
         name=$(cat $config |  jq '."strategy-name"' -r)
         strategy=$(grep "class $name" ./strategies/*.py --files-with-matches)
         cp "$strategy" "output/$config/"
+        cp "$config" "output/$config/"
     fi
   done
 
