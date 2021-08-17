5f67d2c371031191cacf943d7473851e161bff5f
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-08-17 15:19:54 +0200

    [improved] - plot

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ for config in *.json
           work 20190601 $date_format $config false
           work 20200601 $date_format $config true
           mkdir "output/$config/plots"
-          cp ./data/backtesting-data/plots "output/$config/plots/"
+          cp -a ./data/backtesting-data/plots/. "output/$config/plots/"
           work 20210221 20210228 $config false
           work 20210428 20210527 $config false
         fi
