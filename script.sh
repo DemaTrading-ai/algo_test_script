bced37c3c7925dba7eb14536dda76868f934133b
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-08-23 15:34:07 +0200

    [changes] - removing of intermediate plots

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ for config in *.json
           work 20190601 $date_format $config true
           mkdir "output/$config/plots"
           cp -a ./data/backtesting-data/plots/. "output/$config/plots/"
-          rm -r ./data/backtesting-data/plots/.
+          rm -r ./data/backtesting-data/plots/*
           work 20200601 $date_format $config false
           work 20210221 20210228 $config false
           work 20210428 20210527 $config false
