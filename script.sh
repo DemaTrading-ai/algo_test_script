a3745283b178e02f549e0abc9ee64f16a3065e3d
DemaTrading.ai <79081253+dema-trading-ai@users.noreply.github.com>
2021-11-19 14:21:49 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,15 +5,18 @@ work () {
   local to=$2
   local config=$3
   local plots=$4
+  local html=$5
   targetdir=$(pwd)/output/$config
   docker run --pull always -v "$(pwd)/strategies:/usr/src/engine/strategies" \
     -v "$(pwd)/${config}:/usr/src/engine/config.json" \
     -v "$(pwd)/data/backtesting-data:/usr/src/engine/data/backtesting-data" \
-    --rm dematrading/engine:develop -from $from -to $to -plots=$plots |
-#     >>$targetdir/$config.txt
-    aha --black --title 'ls-with-colors' > test_with_colors.html
-#   sed -i 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g' $targetdir/$config.txt
-  cp test_with_colors.html ./output
+    --rm dematrading/engine:develop -from $from -to $to -plots=$plots \
+    >>$targetdir/$config.txt
+  sed -i 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g' $targetdir/$config.txt
+  
+  if[ "$html" = true ] ; then
+    grep --color=always $targetdir/$config.txt | aha --black --title $config > $targetdir/$config.html
+  fi
 }
 
 
@@ -28,15 +31,15 @@ for config in *.json
         mkdir "output/$config"
         if [ -n "$config" ]; then
           echo "BACKTESTING SINGLE CONFIG: ${config}\n\n\n"
-          work 20190601 $date_format $config true
+          work 20190601 $date_format $config true true
           mkdir "output/$config/plots"
           cp -a ./data/backtesting-data/plots/. "output/$config/plots/"
           cp ./data/backtesting-data/trades_log.json "output/$config/"
           sudo rm -r ./data/backtesting-data/plots
           sudo rm ./data/backtesting-data/trades_log.json
-          work 20200601 $date_format $config false
-          work 20210221 20210228 $config false
-          work 20210428 20210527 $config false
+          work 20200601 $date_format $config false false
+          work 20210221 20210228 $config false false
+          work 20210428 20210527 $config false false
         fi
 
         name=$(cat $config |  jq '."strategy-name"' -r)
